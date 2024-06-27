305021a6204127a8c98dc4dc57367c5d5322f41e
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-06-27 10:54:28 +0530

    Update SpecialNumber.java

--- SpecialNumber.java
+++ SpecialNumber.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
-public class SpecialNumber {
-
-    public static void main(String[] args) {
+public class SpecialNumber 
+{
+    public static void main(String[] args) 
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
         scanner.close();
 
-        if (isSpecialNumber(number)) {
+        if (isSpecialNumber(number)) 
+        {
             System.out.println(number + " is a special number.");
-        } else {
+        } 
+        else 
+        {
             System.out.println(number + " is not a special number.");
         }
     }
 
-    public static boolean isSpecialNumber(int number) {
+    public static boolean isSpecialNumber(int number) 
+    {
         int sum = 0;
         int temp = number;
 
-        while (temp != 0) {
+        while (temp != 0) 
+        {
             int digit = temp % 10;
             sum += factorial(digit);
             temp /= 10;
@@ -29,15 +35,16 @@ public class SpecialNumber {
     }
 
     public static int factorial(int n) {
-        if (n == 0) {
+        if (n == 0) 
+        {
             return 1;
         }
 
         int fact = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
+            {
             fact *= i;
         }
-
         return fact;
     }
 }
