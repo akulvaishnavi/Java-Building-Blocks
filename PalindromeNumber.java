9ef0c41744a0633257bd19421f50c788b3ded7ca
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-06-28 14:06:59 +0530

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
-
-public class PalindromeNumber {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class PalindromeNumber 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        scanner.close();
+        int n = sc.nextInt();
+        sc.close();
 
-        int originalNumber = number;
+        int originalNumber = n;
         int reversedNumber = 0;
 
-        for (int temp = number; temp != 0; temp /= 10) {
-            int digit = temp % 10;
+        for (int i = n; i > 0; i /= 10) 
+        {
+            int digit = i % 10;
             reversedNumber = reversedNumber * 10 + digit;
         }
 
-        if (originalNumber == reversedNumber) {
+        if (originalNumber == reversedNumber) 
+        {
             System.out.println(originalNumber + " is a palindrome number.");
-        } else {
+        } 
+        else 
+        {
             System.out.println(originalNumber + " is not a palindrome number.");
         }
     }
