eebd5a3bd161f06ef0f24ea9d97e1f0954b349ac
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-07-05 12:25:43 +0530

    Update SumOfSeries.java

--- SumOfSeries.java
+++ SumOfSeries.java
@@ -1,23 +1,23 @@
 /* Program to show a sum of series like :-
-* 1/4 + 1/8 + 1/12 ...... upto n terms
-*/
-import java.util.Scanner;//Scanner class imported from java.util package
+* 1/4 + 1/8 + 1/12 ...... upto n terms */
+
+import java.util.Scanner; // Scanner class imported from java.util package
 public class SumOfSeries
 {
-public static void main (String args [])// Main method declared
+public static void main (String args []) // Main method declared
 {
-Scanner sc = new Scanner (System.in);//Declaring object to use Scanner class
-System.out.println("Enter Terms");// Asking user for no. of terms
-int n = sc.nextInt();//Accepting value from User
-double sum = 0.0;//Declaring variable sum
-int j = 4;//Declaring variable j
-for (int i = 1; i <=n; i++)//For Loop Declared for taking out the sum
+Scanner sc = new Scanner (System.in); // Declaring object to use Scanner class
+System.out.println("Enter Terms"); // Asking user for no. of terms
+int n = sc.nextInt(); // Accepting value from User
+double sum = 0.0; // Declaring variable sum
+int j = 4; // Declaring variable j
+for (int i = 1; i <=n; i++) //For Loop Declared for taking out the sum
 {
-   sum = sum + (double) 1/j;//sum is equal to sum plus double form of 1/j
-                           //(explicit)
-   j = j + 4;//j is incremented by 4
+   sum = sum + (double) 1/j; // sum is equal to sum plus double form of 1/j
+                            // (explicit)
+   j = j + 4; // j is incremented by 4
 }
-System.out.println("The sum is " + sum);//sum is displayed to user
+System.out.println("The sum is " + sum); // sum is displayed to user
 }
 }
 /* Variable Description table:-
