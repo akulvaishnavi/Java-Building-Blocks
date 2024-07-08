95061cbacaa509796c613faa54cb7f20ebed419b
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-07-08 20:59:19 +0530

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -7,15 +7,14 @@ class Anagrams
 int c = 0;     
 void input()
 {
-Scanner sc = new Scanner(System.in);//Declaring object to use Scanner class
-System.out.print("Enter a word : ");//Asking user for word
-String s = sc.next();//Accepting value from User
-System.out.println("The Anagrams are : ");//Anagrams of word are shown
-display("",s);//here
-System.out.println("Total Number of Anagrams = "+c);//no. of anagrams are
-//shown
+Scanner sc = new Scanner(System.in); // Declaring object to use Scanner class
+System.out.print("Enter a word : "); // Asking user for word
+String s = sc.next(); // Accepting value from User
+System.out.println("The Anagrams are : ");
+display("",s); // Anagrams of word are shown here
+System.out.println("Total Number of Anagrams = "+c); // No. of anagrams are shown
 }   
-void display(String s1, String s2)//function to display anagrams
+void display(String s1, String s2) // Function to display anagrams
 {
 if(s2.length()<=1)
 {
@@ -24,18 +23,18 @@ System.out.println(s1+s2);
 }
 else
 {
-for(int i=0; i<s2.length(); i++)//for loop to find anagrams
+for(int i=0; i<s2.length(); i++) // For loop to find anagrams
 {
 String x = s2.substring(i, i+1);
 String y = s2.substring(0, i);
 String z = s2.substring(i+1);
-display(s1+x, y+z);//Recursive function is called
+display(s1+x, y+z); // Recursive function is called
 }
 }
 }
-public static void main(String args[])//Main method declared
+public static void main(String args[]) // Main method declared
 {
-Anagrams ob=new Anagrams();//Object to use Anagrams class
+Anagrams ob=new Anagrams(); // Object to use Anagrams class
 ob.input();
 }
 }
@@ -43,7 +42,7 @@ ob.input();
  * S.No         Variable Name           Data Type          Description
  *  1                c                     int          Used for counter 
  *                                                      variable to keep 
- *                                                      trac of no. of 
+ *                                                      track of no. of 
  *                                                      anagrams
  *  2                s                   String         Used for storing the
  *                                                      word whose anagrams
