267c3b5b6c6ab562c22ad7e2dd49dfe1d3d38e21
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-07-22 16:25:16 +0530

    Create ArrayRotationACW.java

---
+++ ArrayRotationACW.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ArrayRotationACW
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Elements");
+        int a[][]= new int[3][3]; int m = 3;
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < m/2; i++)
+        {
+            for(int j = 0; j < 3-i-1; j++)
+            {
+               int temp = a[i][j];
+                a[i][j] = a[j][m-1-i];  
+                a[j][m-1-i] = a[m-1-i][m-1-j]; 
+                a[m-1-i][m-1-j] = a[m-1-j][i];
+                a[m-1-j][i]= temp; 
+            }
+        }
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
