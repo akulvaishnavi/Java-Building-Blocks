d76f3164ca0afcc0f52917407e011736a69d01d0
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-08-16 08:17:18 +0530

    Create Nested.java

---
+++ Nested.java
@@ -0,0 +1,15 @@
+public class Nested
+{
+   public static void main(String args[])
+   {
+       int n = 1;
+       for (int i = 6; i >=n; i--)
+       {
+           for (int j = 1; j <=i; j++)
+           {
+               System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+}
+}
