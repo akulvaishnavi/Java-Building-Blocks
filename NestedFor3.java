d85a6f0a511e21254407fa3adc7961a1ec1ffe6b
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-08-16 08:18:16 +0530

    Create NestedFor3.java

---
+++ NestedFor3.java
@@ -0,0 +1,15 @@
+public class NestedFor3
+{
+    public static void main (String args [])
+    {
+        int n = 9;
+    for (int i = 9;i >=1; i--)
+    {
+        for (int j = 9; j >= i; j--)
+        {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+}
+}
+}
