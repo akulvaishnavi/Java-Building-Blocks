84f3bccb3db2bb680d3225e376af7ca949387a2e
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-08-16 08:17:45 +0530

    Create NestedFor2.java

---
+++ NestedFor2.java
@@ -0,0 +1,15 @@
+public class NestedFor2
+{
+    public static void main (String args [])
+    {
+        int n = 9;
+    for (int i = 9;i >=1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+}
+}
+}
