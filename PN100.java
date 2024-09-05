a28c770f3b9eee66290d46464e616db70d77deec
Akul Vaishnavi <akulvaishnavi1@gmail.com>
2024-09-05 16:16:53 +0530

    Update PN100.java

--- PN100.java
+++ PN100.java
@@ -3,11 +3,11 @@ public class PN100
     public static void main (String args [])
     {
         int c = 0;
-        int t = 0;;
+        int t = 0;
         int sum = 0;
-        for (int i = 1;i <=100; i++)
+        for (int i = 1;i <= 100; i++)
         {
-            for (int l = 1; l <=i; l++)
+            for (int l = 1; l <= i; l++)
        {
         if (i / l == 1)
         {
